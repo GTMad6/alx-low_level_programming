283e2ab3589eec03ff61cc1b50ce2d439286e9e8
Guide Madembo <guidemad@gmail.com>
2023-11-06 19:14:48 -0800

    Poppy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,22 @@
+#ifndef DOG_H_
+#define DOG_H_
+
+/**
+ * struct dog - a dog's basic info
+ *
+ * @name: the name of the dog
+ * @age: the age of the dog
+ * @owner: the owner of the dog
+ *
+ * Description: first struct with dog
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
+
